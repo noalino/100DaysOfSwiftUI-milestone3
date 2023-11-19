1473c6c4fae4b594489803bfb0d1fab67460b681
Noalino <bgelineau.dev@gmail.com>
2023-11-19 10:12:02 +0100

    Initial Commit

---
+++ HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Noalino on 19/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
